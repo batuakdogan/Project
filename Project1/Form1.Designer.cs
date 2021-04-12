4723c92faa4c03061812f4bb6774be887f5071e0
batcue <bat.gsakd@gmail.com>
2021-04-12 15:52:27 +0300

    Add project files.

---
+++ Project1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxProducts = new System.Windows.Forms.ListBox();
+            this.lblProducts = new System.Windows.Forms.Label();
+            this.btnAddToCard = new System.Windows.Forms.Button();
+            this.lbxCart = new System.Windows.Forms.ListBox();
+            this.lblCart = new System.Windows.Forms.Label();
+            this.btnRemoveFromCart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbxProducts
+            // 
+            this.lbxProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbxProducts.FormattingEnabled = true;
+            this.lbxProducts.ItemHeight = 17;
+            this.lbxProducts.Location = new System.Drawing.Point(76, 84);
+            this.lbxProducts.Name = "lbxProducts";
+            this.lbxProducts.Size = new System.Drawing.Size(136, 259);
+            this.lbxProducts.TabIndex = 0;
+            // 
+            // lblProducts
+            // 
+            this.lblProducts.AutoSize = true;
+            this.lblProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblProducts.Location = new System.Drawing.Point(72, 48);
+            this.lblProducts.Name = "lblProducts";
+            this.lblProducts.Size = new System.Drawing.Size(0, 20);
+            this.lblProducts.TabIndex = 1;
+            // 
+            // btnAddToCard
+            // 
+            this.btnAddToCard.Location = new System.Drawing.Point(248, 95);
+            this.btnAddToCard.Name = "btnAddToCard";
+            this.btnAddToCard.Size = new System.Drawing.Size(127, 33);
+            this.btnAddToCard.TabIndex = 2;
+            this.btnAddToCard.Text = "button1";
+            this.btnAddToCard.UseVisualStyleBackColor = true;
+            this.btnAddToCard.Click += new System.EventHandler(this.btnAddToCard_Click);
+            // 
+            // lbxCart
+            // 
+            this.lbxCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbxCart.FormattingEnabled = true;
+            this.lbxCart.ItemHeight = 17;
+            this.lbxCart.Location = new System.Drawing.Point(586, 84);
+            this.lbxCart.Name = "lbxCart";
+            this.lbxCart.Size = new System.Drawing.Size(154, 259);
+            this.lbxCart.TabIndex = 3;
+            this.lbxCart.SelectedIndexChanged += new System.EventHandler(this.lbxCart_SelectedIndexChanged);
+            // 
+            // lblCart
+            // 
+            this.lblCart.AutoSize = true;
+            this.lblCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCart.Location = new System.Drawing.Point(586, 48);
+            this.lblCart.Name = "lblCart";
+            this.lblCart.Size = new System.Drawing.Size(53, 20);
+            this.lblCart.TabIndex = 4;
+            this.lblCart.Text = "label1";
+            // 
+            // btnRemoveFromCart
+            // 
+            this.btnRemoveFromCart.Location = new System.Drawing.Point(772, 95);
+            this.btnRemoveFromCart.Name = "btnRemoveFromCart";
+            this.btnRemoveFromCart.Size = new System.Drawing.Size(123, 33);
+            this.btnRemoveFromCart.TabIndex = 5;
+            this.btnRemoveFromCart.Text = "button1";
+            this.btnRemoveFromCart.UseVisualStyleBackColor = true;
+            this.btnRemoveFromCart.Click += new System.EventHandler(this.btnRemoveFromCart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 562);
+            this.Controls.Add(this.btnRemoveFromCart);
+            this.Controls.Add(this.lblCart);
+            this.Controls.Add(this.lbxCart);
+            this.Controls.Add(this.btnAddToCard);
+            this.Controls.Add(this.lblProducts);
+            this.Controls.Add(this.lbxProducts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxProducts;
+        private System.Windows.Forms.Label lblProducts;
+        private System.Windows.Forms.Button btnAddToCard;
+        private System.Windows.Forms.ListBox lbxCart;
+        private System.Windows.Forms.Label lblCart;
+        private System.Windows.Forms.Button btnRemoveFromCart;
+    }
+}
+
---
+++ Project1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string productsText = "Ürünler";
+
+
+            string addToCartButtonText = "Sepete ekle";
+            string cartText = "Sepetiniz";
+
+            var removeFromCartButtonText = "Sepetten çıkar";
+
+
+            lbxProducts.Text = productsText;
+
+            btnAddToCard.Text = addToCartButtonText;
+
+            lblCart.Text = cartText;
+            btnRemoveFromCart.Text = removeFromCartButtonText;
+
+
+            lblProducts.Text = "Ürünler";
+
+
+
+
+
+            string[] products = new string[] { "Laptop", "Masaüstü PC", "Klavye" };
+
+
+
+
+            // for (int i = 0; i < products.Length; i++)
+            //{
+            //   lbxProducts.Items.Add(products[i]);
+
+            //}
+
+
+            foreach (var item in products)
+            {
+                lbxProducts.Items.Add(item);
+            }
+
+            if (lbxCart.Items.Count == 0)
+            {
+                btnRemoveFromCart.Enabled = false;
+            }
+
+
+
+
+
+        }
+
+        private void btnAddToCard_Click(object sender, EventArgs e)
+        {
+
+            if (lbxProducts.SelectedItem != null)
+            {
+                lbxCart.Items.Add(lbxProducts.SelectedItem);
+                btnRemoveFromCart.Enabled = true;
+
+            }
+            else
+            {
+                MessageBox.Show("Ürün seçin!");
+            }
+
+        }
+
+        private void lbxCart_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRemoveFromCart_Click(object sender, EventArgs e)
+        {
+            if (lbxCart.SelectedItem!=null)
+            {
+                lbxCart.Items.Remove(lbxCart.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Öncelikle ürün seçmelisiniz");
+            }
+
+            
+
+
+            if (lbxCart.Items.Count ==0)
+            {
+                btnRemoveFromCart.Enabled = false;
+            }
+
+        }
+    }
+}
